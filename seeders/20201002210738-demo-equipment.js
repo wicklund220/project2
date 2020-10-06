bca7a8882654d45a83e8997a0d73d33914aa7e86
cyoder1 <yodercamdend@johndeere.com>
2020-10-06 13:27:29 -0500

    added equipment seeder data

--- seeders/20201002210738-demo-equipment.js
+++ seeders/20201002210738-demo-equipment.js
@@ -33,7 +33,56 @@ module.exports = {
       horsepower: 38,
       description: 'Turf and Utility applications, property management',
       img: 'https://i.imgur.com/kTbpfjAm.jpg'
-    }], {});
+    },
+    {
+      division: 'agriculture',
+      modelName: '6250R',
+      application: 'Utility',
+      horsepower: 250,
+      description: 'Livestock farming, loader work, mid size specialty applications',
+      img: 'https://i.imgur.com/uHV4o5om.jpg'
+    },
+    {
+      division: 'agriculture',
+      modelName: '5115ML',
+      application: 'Specialty',
+      horsepower: 115,
+      description: 'orchard applications',
+      img: 'https://i.imgur.com/cthEo06m.jpg'
+    },
+    {
+      division: 'Construction',
+      modelName: '870G',
+      application: 'Excavation',
+      horsepower: 512,
+      description: 'Used for digging pits, foundations, or other construction work',
+      img: 'https://i.imgur.com/hxZTTgBm.jpg'
+    },
+    {
+      division: 'Construction',
+      modelName: '872G',
+      application: 'Earth Grading',
+      horsepower: 300,
+      description: 'Leveling roadways, large areas of land, cutting ditches',
+      img: 'https://i.imgur.com/hFl8ISbm.jpg'
+    },
+    {
+      division: 'Construction',
+      modelName: '460E',
+      application: 'Material Transport',
+      horsepower: 481,
+      description: 'Used to transport large quantites of material at mine and other excavating sites',
+      img: 'https://i.imgur.com/KcwUGv8m.jpg'
+    },
+    {
+      division: 'Agriculture',
+      modelName: 'X9 1100',
+      application: 'Grain Harvesting',
+      horsepower: 690,
+      description: 'Used to harvest grain',
+      img: 'https://i.imgur.com/6j7xpkQm.jpg'
+    },
+  ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20201002210754-demo-attachments.js
+++ seeders/20201002210754-demo-attachments.js
@@ -7,8 +7,30 @@ module.exports = {
       modelName: '440R',
       application: 'Tractor',
       description: 'utility loading purposes',
-      img: 'https://www.deere.com/assets/images/region-4/products/loaders/front-end-loaders/440r/440r_r4g032999_72dpi_rrd_1366x768_large_009dba650fc4cb4ee16f52353f498dfedc63ec5e.jpg'
-    }], {});
+      img: 'https://i.imgur.com/4k3TqoJm.jpg'
+    },
+    {
+      division: 'agriculture',
+      modelName: 'HD50R',
+      application: 'Draper Platform',
+      description: 'Used on a combine for harvesting small grains',
+      img: 'https://i.imgur.com/neWOWOUm.jpg'
+    },
+    {
+      division: 'agriculture',
+      modelName: 'C18F',
+      application: 'Corn head',
+      description: 'Used on a combine for harvesting corn',
+      img: 'https://i.imgur.com/EyKMrbym.jpg'
+    },
+    {
+      division: 'agriculture',
+      modelName: '2660VT',
+      application: 'Vertical tillage tool',
+      description: 'Multiseasonal tillage, turning over soil',
+      img: 'https://i.imgur.com/cZ5EXWhm.jpg'
+    },
+  ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
